0f12bdc449c733b7acd84da50e4be8e3802025a8
DerronOnefive <111064439+DerronOnefive@users.noreply.github.com>
2024-03-18 23:29:27 -0400

    Update Movement.java

--- src/Movement.java
+++ src/Movement.java
@@ -1,3 +1,10 @@
+/**
+ * ClassName:Movement
+ * @Author:Derron
+ * @Version:1.0
+ * Course:ITEC 2140
+ * Date: 3/18/2024
+ */
 // Interface defining the movement behavior, extending the Flight interface
 interface Movement extends Flight {
     void walk(); // Method to perform walking action
