a46d205767b7e17ada3f775e3f375d31d9774754
DerronOnefive <111064439+DerronOnefive@users.noreply.github.com>
2024-03-18 23:29:08 -0400

    Update Flight.java

--- src/Flight.java
+++ src/Flight.java
@@ -1,3 +1,10 @@
+/**
+ * ClassName:Flight
+ * @Author:Derron
+ * @Version:1.0
+ * Course:ITEC 2140
+ * Date: 3/18/2024
+ */
 // Interface defining the flight behavior
 interface Flight {
     void fly(); // Method to perform flying action
